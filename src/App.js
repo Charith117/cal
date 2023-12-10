6b27fc49ac1343b08647a09733907acb193c3294
Charith Jayanitha <123439615+Charith117@users.noreply.github.com>
2023-12-10 10:47:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import Calculator from './CAlculator';
+
+const App = () => {
+  return (
+    <div className="app">
+      <center><h1>React Calculator</h1> </center>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
